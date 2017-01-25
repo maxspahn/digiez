fbabc9315e5271eb586776b61773a6b011c6a7e1
maxspahn <maxspahn3@yahoo.de>
2017-01-25 18:24:31 +0100

    Modified the code after having received the video from the company. In fact the photos are mixed up as well so I decided to compare all images and put those one after another which have the least difference. It's a kind of brute force but it is working very well if the pixels are read at the beginning and not in the loop. Some notification missing.

--- digiez.cpp
+++ digiez.cpp
@@ -4,37 +4,94 @@
 #include "bmp.h"
 #include <iostream>
 #include "EasyBMP.h"
+
 using namespace std;
 
-int** getPixels(BMP image,int div,  int a, int b){
-	int** values = new int*[div*div];
+/// @brief Get the color of a given number of pixel and a given image.
+//
+/// @param image 	The bitmap of the image to be evaluated.
+/// @param div 		The number of division in every direction.
+/// @param a 		Number of pixels in between two pixels that are evaluated, horizontal direction.
+/// @param b 		Number of pixels in between two pixels that are evaluated, vertical direction.
+//
+/// @return The values of all the pixeles evaluated and every color.
+
+int* getPixels(BMP image,int div,  int a, int b){
+	int* values = new int[div*div*3];
 	int i = 0;
 	for (int c = 1; c <= div; c++) {
 		for (int d = 1; d <= div; d++) {
-			values[i] = new int[3];
-			values[i][0] = image(c*a, d*b)->Red;
-			values[i][1] = image(c*a, d*b)->Green;
-			values[i][2] = image(c*a, d*b)->Blue;
-			i++;
+			values[i]   = image(c*a, d*b)->Red;
+			values[i+1] = image(c*a, d*b)->Green;
+			values[i+2] = image(c*a, d*b)->Blue;
+			i+=3;
 		}
 	}
 	return values;
 
 };
 
-int getSumDiff(int nbPnt, int** first, int** second){
+/// @brief Get the values of all colors of every pixel for all the images.
+//
+/// @param images 	A list of all the images to be evaluated.
+/// @param div 		Number of division on one direction.
+/// @param a 		Number of pixels in between two pixels that are evaluated, horizontal direction.
+/// @param b 		Number of pixels in between two pixels that are evaluated, vertical direction.
+/// @param nbIm 	Number of imagese in the list.
+//
+/// @return A two dimensional array with all the values.
+
+int** getAllPixels(BMP * images, int div, int a, int b, int nbIm){
+	int** colors = new int*[nbIm];
+	for (int i = 0; i < nbIm; i++) {
+		colors[i] = getPixels(images[i], div, a, b);
+	}
+	return colors;
+};
+
+/// @brief Compute the sum of differences for all the pixels between two images.
+//
+/// @param nbPnt 	Number of points to be evaluated.
+/// @param first 	Array containing the color values for the first image.
+/// @param second 	Array containing the color values for the second image.
+//
+/// @return The sum of differences.
+
+int getSumDiff(int nbPnt, int* first, int* second){
 	int diff = 0;
-	for (int i = 0; i < nbPnt; i++) {
-		for (int a = 0; a < 3; a++) {
-			diff +=abs(first[i][a]-second[i][a]);
-		}
+	for (int i = 0; i < nbPnt*3; i++) {
+		diff +=abs(first[i]-second[i]);
 	}
 	return diff;
 
 };
 
+int compare2Im(int* a, int* b, int div){
+	return getSumDiff(div*div,a,b);
+	
+};
+
+/// @brief Read all images that have been created by they framing into one array of bitmaps.
+//
+/// @param nbIm Number of images.
+//
+/// @return An array of all the images.
+
+BMP* readAllImages(int nbIm){
+	BMP* imList = new BMP[nbIm];
+	char* name = new char[32];
+	for (int i = 1; i <= nbIm; i++) {
+		sprintf(name, "images/out%d.bmp", i);
+		imList[i-1].ReadFromFile(name);
+	}
+	delete [] name;
+	return imList;
+};
+
 int main(int argc, const char *argv[])
 {
+
+	// line 95-105: getting information concerning the image size and computing the number of points
 	BMP im;
 	im.ReadFromFile("images/out1.bmp");
 	int width = im.TellWidth();
@@ -46,78 +103,56 @@ int main(int argc, const char *argv[])
 
 	int w = width/(nbDiv+1);
 	int h = height/(nbDiv+1);
-	int diff;
-	char* name1= new char[32];
-	char* name2 = new char[32];
-	char* falseIm = new char[32];
-	char * trueIm = new char[32];
-
-	int error, index = 1, j = 1;
-	BMP im1;
-	BMP im2;	
-
-	for (int i = 2; i < 200; i++) {
-		int a = i-1;
-		sprintf(name1, "images/out%d.bmp", a);
-		im1.ReadFromFile(name1);
-		sprintf(name2, "images/out%d.bmp", i);
-		im2.ReadFromFile(name2);
-		
-
-		int** pi1 = getPixels(im1,nbDiv, w,h);
-		int** pi2 = getPixels(im2,nbDiv ,w,h);
-
-		diff = getSumDiff(points, pi1, pi2);
-		printf("%d\n", diff);
-		/*
-		for (int i = 0; i < points; i++) {
-			printf("Differences in point %d\n", i);
-			for (int a = 0; a < 3; a++) {
-				printf("%d  ", pi1[i][a]-pi2[i][a]);
-			}
-			printf("\n");
-		}
-		*/
-		sprintf(trueIm, "images/new%03d.bmp", i-index);
-		error = rename(name1,trueIm);
-
-		if (diff>10*points) {
-			i++;
-			printf("%s\n", name2);
-			sprintf(falseIm, "images/false/false%d.bmp", index);
-			error = rename(name2,falseIm);
-			index++;
-		}
-		for (int i = 0; i < points; i++) {
-			delete [] pi1[i];
-			delete [] pi2[i];
-		}
-		delete [] pi1;
-		delete [] pi2;
-			
+	
+	
+	
+	
+	int end = atoi(argv[2])-3;
+	
+	int * changings = new int[end];
+	for (int i = 0; i < end; i++) {
+		changings[i] = i;
 	}
 
-
-	delete [] name1;
-	delete [] name2;
-	delete [] falseIm;
-	delete [] trueIm;
-	
+	//set first image
+	//
+	changings[0] = 30;
+	changings[30] = 0;
 
 
+	BMP* list = readAllImages(end);
+	int** col = getAllPixels(list, nbDiv, w, h, end);
 
 
-
-
-
-
-
-
-
-
-
-
-
+	int temp;
+	int diff, min, index;
+	for (int i = 0; i < end-1; i++) {
+		min = 100000000;
+		for (int a = i+1; a < end-1; a++) {
+			diff  =	compare2Im(col[changings[i]], col[changings[a]], nbDiv);
+			printf("difference between %d and %d : %d\n", i, a , diff);
+			if(diff < min){
+				index = a;
+				min = diff;
+			}	
+		}
+		temp = changings[i+1];
+		changings[i+1] = changings[index];
+		changings[index] = temp;
+		
+		//printf("Difference between %d and %d : %d\n",i+1, i+2,diff1);
+	}
+	for (int i = 0; i < end; i++) {
+		printf("%d\n", changings[i]+1);
+	}
+	char * name = new char[32];
+	char * newname = new char[32];
+	int error;
+	for (int i = 0; i < end; i++) {
+		sprintf(name, "images/out%d.bmp",changings[i]+1);
+		sprintf(newname, "images/new%03d.bmp", i);
+		error = rename(name, newname);
+	}
 
 	return 0;
 }
--- video.sh
+++ video.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/sh
+video = $1
+
+echo "Compiling false Image detection"
+g++ -o d digiez.cpp EasyBMP.cpp
 
 
-echo "Executing false Image detection"
+
+ffmpeg -i "$1" -vf fps=25 ./images/out%d.bmp
+
+echo "counting photos"
+numfiles=$(ls ./images -a | wc -l)
+echo $numfiles
+
+echo "Finding and move bad images"
+./d 10 $numfiles
+
+ffmpeg -r 25 -f image2 -i ./images/new%03d.bmp -vcodec libx265 -crf 25  -pix_fmt yuv420p ./Corrected.mkv
 
 
-ffmpeg -i ~/Videos/natureCut.mkv -vf fps=24 ~/Pictures/video/images/out%d.bmp
-
-
-cd ~/Pictures/video
-./d 10
-
-ffmpeg -r 24 -f image2 -i ~/Pictures/video/images/new%03d.bmp -vcodec libx265 -crf 25  -pix_fmt yuv420p ~/Videos/test.mp4
-
-
-rm ~/Pictures/video/images/new*
-rm ~/Pictures/video/images/out*
+rm ./images/new*
 
